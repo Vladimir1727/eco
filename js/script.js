be8191db9e3e7fb06b3a84a58510b2b402a62ec3
Unknown <dvn125@gmail.com>
2017-08-22 13:29:28 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,41 @@
+(function($){$(function(){
+
+console.log('repetitor profile 6');
+
+/*$('#slide').click(function(){
+    $('#slide ul').slideToggle();
+});*/
+
+$('#rep-online').click(function(){
+    var s = $('#rep-online');
+    if (s.hasClass('on')){
+        s.removeClass('on');
+        s.addClass('off');
+    } else {
+        s.removeClass('off');
+        s.addClass('on');
+    }
+});
+
+function warp(block){
+    $('section.warp aside').each(function(){
+        $(this).hide();
+    });
+    $(block).show();
+    $('main.rep-profile section.menu li a').each(function(){
+        $(this).removeClass('active');
+    });
+    $(block+'-but').addClass('active');
+}
+
+$('#present-but').click(function(){
+    warp('#present');
+    return false;
+});
+
+$('#personal-but').click(function(){
+    warp('#personal');
+    return false;
+});
+
+})})(jQuery)
